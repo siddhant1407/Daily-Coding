0101f5a3c9b1c87000662c13fe78f835d687ed50
Patil, Siddhant <sidhantpatil04@gmail.com>
2024-04-08 09:45:50 -0400

    LC Kids With the Greatest Number of Candies (Easy - 1431) java

--- LC/LC75/Arrays_String/Java/ArrayString.java
+++ LC/LC75/Arrays_String/Java/ArrayString.java
@@ -5,16 +5,21 @@ public class ArrayString {
     public static void main (String args[]){
         // // 1. Merge Alternately (Easy- 1768)
         Scanner sc = new Scanner(System.in);
-        System.out.print("Enter the String1: ");
-        String s1 = sc.nextLine();
-        System.out.print("Enter the String2: ");
-        String s2 = sc.nextLine();
-        sc.close();
+        // System.out.print("Enter the String1: ");
+        // String s1 = sc.nextLine();
+        // System.out.print("Enter the String2: ");
+        // String s2 = sc.nextLine();    
         // System.out.println("The joined string is: " + mergeAlternately(s1, s2));
 
-        // 2.  Greatest Common Divisor of Strings (1071)
-        System.out.println("The GCD of String is: " + gcdOfString(s1, s2));
+        // 2.  Greatest Common Divisor of Strings (Easy- 1071)
+        // System.out.println("The GCD of String is: " + gcdOfString(s1, s2));
 
+        // 3. Kids With the Greatest Number of Candies (Easy - 1431)
+        int [] candies = {2,3,5,1,3};
+        int  extraCandies = 3;
+        System.out.println("The Boolean array denoting the result is: " + kidsWithCandies(candies, extraCandies));
+
+        sc.close();
     }
 
     // 1. Merge Alternately (Easy- 1768)
@@ -33,12 +38,11 @@ public class ArrayString {
         return sb.toString();
     }
 
-    // 2.  Greatest Common Divisor of Strings (1071)
+    // 2.  Greatest Common Divisor of Strings (Easy- 1071)
     public static String gcdOfString(String str1, String str2) {
         if (!(str1+str2).equals(str2+str1)){
             return "";
         }
-
         while(!str1.equals(str2)) {
             if (str1.length() > str2.length()) {
                 str1 = str1.substring(str2.length());
@@ -47,6 +51,25 @@ public class ArrayString {
             }
         }
         return str1;
+    }
+
+    // 3. Kids With the Greatest Number of Candies (Easy - 1431)
+    public static List<Boolean> kidsWithCandies(int [] candies, int extraCandies){
+        List<Boolean> result = new ArrayList<>();
+        int max_num = 0;   //you can set this to Integer.MIN_VALUE; (for the lowest int value initialization)
+
+        for (int candy : candies){
+            max_num = Math.max(max_num,candy);
+        }
+
+        for (int candy: candies){
+            if (candy + extraCandies >= max_num){           // result.add(candy + extraCandies >= maxNum); ~ in one line.
+                result.add(true);
+            }else{
+                result.add(false);
+            }
+        }
+        return result;
     }
 
 }
