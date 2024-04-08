5a5ae1f9e7adde8df1e162ebfda8be54e06776c0
Patil, Siddhant <sidhantpatil04@gmail.com>
2024-04-08 09:27:53 -0400

    LC Greatest Common Divisor of Strings (1071)

--- LC/LC75/Arrays_String/Java/ArrayString.java
+++ LC/LC75/Arrays_String/Java/ArrayString.java
@@ -3,12 +3,18 @@ import java.util.*;
 
 public class ArrayString {
     public static void main (String args[]){
-        // 1. Merge Alternately (Easy- 1768)
+        // // 1. Merge Alternately (Easy- 1768)
         Scanner sc = new Scanner(System.in);
+        System.out.print("Enter the String1: ");
         String s1 = sc.nextLine();
+        System.out.print("Enter the String2: ");
         String s2 = sc.nextLine();
         sc.close();
-        System.out.println("The joined string is: " + mergeAlternately(s1, s2));
+        // System.out.println("The joined string is: " + mergeAlternately(s1, s2));
+
+        // 2.  Greatest Common Divisor of Strings (1071)
+        System.out.println("The GCD of String is: " + gcdOfString(s1, s2));
+
     }
 
     // 1. Merge Alternately (Easy- 1768)
@@ -27,5 +33,21 @@ public class ArrayString {
         return sb.toString();
     }
 
+    // 2.  Greatest Common Divisor of Strings (1071)
+    public static String gcdOfString(String str1, String str2) {
+        if (!(str1+str2).equals(str2+str1)){
+            return "";
+        }
+
+        while(!str1.equals(str2)) {
+            if (str1.length() > str2.length()) {
+                str1 = str1.substring(str2.length());
+            } else {
+                str2 = str2.substring(str1.length());
+            }
+        }
+        return str1;
+    }
 
 }
+
