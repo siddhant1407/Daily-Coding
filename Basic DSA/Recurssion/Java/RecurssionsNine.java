e4e032b70bc9e7d335df3a11518c149c6508fd1f
Patil, Siddhant <sidhantpatil04@gmail.com>
2024-04-10 09:06:50 -0400

    Recurssion Q2. Print a string in reverse.

--- Basic DSA/Recurssion/Java/RecurssionsNine.java
+++ Basic DSA/Recurssion/Java/RecurssionsNine.java
@@ -1,14 +1,19 @@
 public class RecurssionsNine {
     public static void main (String[] args) {
 
-        // Q1. Tower of Hanoi - Transfer n disks from source to destination over 3 towers.
-        int n = 3;
-        char source = 'A', destination = 'C', helper = 'B';
-        System.out.println("The Steps/Moves for TOH probem begin");
-        towerOfHanoi(n,source,destination,helper);
+        // // Q1. Tower of Hanoi - Transfer n disks from source to destination over 3 towers.
+        // int n = 3;
+        // char source = 'A', destination = 'C', helper = 'B';
+        // System.out.println("The Steps/Moves for TOH probem begin");
+        // towerOfHanoi(n,source,destination,helper);
+        
+
+        // Q2. Print a string in reverse.
+        String str = "HelloWorld";
+        printRev(str, str.length()-1);
+
     }
     
-
     // Q1. Tower of Hanoi - Transfer n disks from source to destination over 3 towers.
     public static void towerOfHanoi(int n, char source, char destination, char helper){
         if(n==1){
@@ -23,5 +28,16 @@ public class RecurssionsNine {
 
         // Move n-1 disks from B to C using A as auxiliary
         towerOfHanoi(n-1, helper, destination, source);
+    }
+
+
+    // Q2. Print a string in reverse.
+    public static void printRev(String s, int idx){
+        if (idx == 0){
+            System.out.print(s.charAt(idx));
+            return;
+        }
+        System.out.print(s.charAt(idx));
+        printRev(s, idx-1);
     }
 }
