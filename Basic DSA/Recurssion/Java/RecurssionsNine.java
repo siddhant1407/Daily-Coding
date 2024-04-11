05afc0df4933d37ad777723721244c6dedc510d1
Patil, Siddhant <sidhantpatil04@gmail.com>
2024-04-11 08:29:08 -0400

    Recurssion Q3 (occurrence of first and last element in str), Q4 (if array is sorted (strictly increasing)).

--- Basic DSA/Recurssion/Java/RecurssionsNine.java
+++ Basic DSA/Recurssion/Java/RecurssionsNine.java
@@ -8,9 +8,26 @@ public class RecurssionsNine {
         // towerOfHanoi(n,source,destination,helper);
         
 
-        // Q2. Print a string in reverse.
-        String str = "HelloWorld";
-        printRev(str, str.length()-1);
+        // // Q2. Print a string in reverse.
+        // String str = "HelloWorld";
+        // printRev(str, str.length()-1);
+
+
+        // // Q3. Find the occurrence of the first and last occurrence of an element using recursion.
+        // String str = "abafhodijdah";
+        // char elem = 'a';
+        // int idx = 0;
+        // findFirstLastOccurrenceRecursively(str,elem, idx, -1, -1);
+
+
+        // Q4. Check if an array is sorted (strictly increasing). - O(n)
+        int [] inputs = {1, 2, 3, 4, 5};
+        boolean result = isSortedStrictlyIncreasing(inputs, 0);
+        if (result == true){
+            System.out.println("Array is strictly increasing.");
+        } else{
+            System.out.println("Array is NOT strictly increasing.");
+        }
 
     }
     
@@ -40,4 +57,39 @@ public class RecurssionsNine {
         System.out.print(s.charAt(idx));
         printRev(s, idx-1);
     }
+
+
+    // Q3. Find the occurrence of the first and last occurrence of an element using recursion.
+    public static void findFirstLastOccurrenceRecursively(String str, char elem, int idx, int first, int last){
+        // base cases
+        if (idx == str.length()){
+            System.out.println("The First  Occurrence is: "+first) ;
+            System.out.println("The Last  Occurrence is: "+last) ;
+            return;
+        }
+        if(str.charAt(idx) == elem){
+            if(first == -1){
+                first = idx;
+            }
+            else{
+                last = idx;
+            }
+        }
+        findFirstLastOccurrenceRecursively(str, elem, idx+1, first, last);
+    }
+
+
+    // Q4. Check if an array is sorted (strictly increasing). - O(n)
+    public static boolean isSortedStrictlyIncreasing(int [] arr, int idx){
+        if  (idx == arr.length - 1){
+            return true;
+        }
+        if (arr[idx] < arr[idx+1]){
+            return isSortedStrictlyIncreasing(arr, idx+1);
+        }
+        else{
+            return false;
+        }
+    }
+
 }
