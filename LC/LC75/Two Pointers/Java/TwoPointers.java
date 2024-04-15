f6a66aeeb78b97c8ec6c3acc7bb7c9e10da22f4a
Patil, Siddhant <sidhantpatil04@gmail.com>
2024-04-15 09:48:56 -0400

    LC Is Subsequence (Medium - 392)

--- LC/LC75/Two Pointers/Java/TwoPointers.java
+++ LC/LC75/Two Pointers/Java/TwoPointers.java
@@ -1,15 +1,6 @@
-import java.util.Arrays;
+// import java.util.Arrays;
 
 public class TwoPointers {
-    public static void main (String[] args) {
-
-        // 1. Move Zeroes (Easy- 283)
-        int nums[] = {0, 1, 0, 3,  12};
-        int result [] = moveZeros(nums);
-        System.out.println("The Array after moving all zeros is: "+Arrays.toString(result));
-
-
-    }
 
     // 1. Move Zeroes (Easy- 283)
     public static int [] moveZeros(int[] nums){
@@ -24,6 +15,34 @@ public class TwoPointers {
             }
         }
         return nums;
+
+    }
+
+    // Is Subsequence (Medium - 392)
+    public static boolean isSubsequence(String s, String t) {
+        int ptr_s = 0;
+        int ptr_t = 0;
+
+        while ((ptr_s < s.length()) && (ptr_t < t.length())) {
+            if (s.charAt(ptr_s) == t.charAt(ptr_t)) {
+                ptr_s += 1;
+            }
+            ptr_t += 1;
+        }
+        return ptr_s == s.length();
+    }
+    
+    public static void main (String[] args) {
+
+        // // 1. Move Zeroes (Easy- 283)
+        // int nums[] = {0, 1, 0, 3,  12};
+        // int result [] = moveZeros(nums);
+        // System.out.println("The Array after moving all zeros is: "+Arrays.toString(result));
+
+
+        // Is Subsequence (Medium - 392)
+        String s = "axc"; String  t = "ahbgdc";
+        System.out.println("Is \""+s+"\" a subsequence of \""+t+"\"?~ "+isSubsequence(s, t));
 
     }
 }
