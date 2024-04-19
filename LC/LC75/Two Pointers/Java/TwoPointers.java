7ba842c9415c5a991b6cd97912ba25564ab47efb
Patil, Siddhant <sidhantpatil04@gmail.com>
2024-04-19 09:31:31 -0400

    LC Max Number of K-Sum Pairs (Med 1679)

--- LC/LC75/Two Pointers/Java/TwoPointers.java
+++ LC/LC75/Two Pointers/Java/TwoPointers.java
@@ -1,9 +1,9 @@
-// import java.util.Arrays;
+import java.util.Arrays;
 
 public class TwoPointers {
 
     // 1. Move Zeroes (Easy- 283)
-    public static int [] moveZeros(int[] nums){
+    public static int[] moveZeros(int[] nums) {
         int left = 0;
         for (int right = 0; right < nums.length; right++) {
             if (nums[right] != 0) {
@@ -32,42 +32,68 @@ public class TwoPointers {
         return ptr_s == s.length();
     }
 
-
-    // Container With Most Water  (Med  11)
-    public static int maxArea(int [] heights){
+    // Container With Most Water (Med 11)
+    public static int maxArea(int[] heights) {
         int left_ptr = 0;
-        int right_ptr = heights.length -1;
+        int right_ptr = heights.length - 1;
         int finalMaxArea = Integer.MIN_VALUE;
 
-        while (left_ptr < right_ptr){
+        while (left_ptr < right_ptr) {
             int area = (right_ptr - left_ptr) * Math.min(heights[left_ptr], heights[right_ptr]);
             finalMaxArea = Math.max(finalMaxArea, area);
 
-            if  (heights[left_ptr] < heights [right_ptr]){
+            if (heights[left_ptr] < heights[right_ptr]) {
                 left_ptr++;
-            }else{
+            } else {
                 right_ptr--;
             }
         }
         return finalMaxArea;
     }
 
+    // Max Number of K-Sum Pairs (Med 1679)
+    public static int maxOperations(int k, int[] nums) {
+        Arrays.sort(nums);
+        int leftPtr = 0;
+        int rightPtr = nums.length - 1;
+        int counter = 0;
 
-    public static void main (String[] args) {
+        while (leftPtr < rightPtr) {
+            int sum = nums[leftPtr] + nums[rightPtr];
+            if (sum == k) {
+                counter++;
+                leftPtr++;
+                rightPtr--;
+            } else if (sum > k) {
+                rightPtr--;
+            } else {
+                leftPtr++;
+            }
+        }
+        return counter;
+    }
+
+    public static void main(String[] args) {
 
         // // 1. Move Zeroes (Easy- 283)
-        // int nums[] = {0, 1, 0, 3,  12};
+        // int nums[] = {0, 1, 0, 3, 12};
         // int result [] = moveZeros(nums);
-        // System.out.println("The Array after moving all zeros is: "+Arrays.toString(result));
-
+        // System.out.println("The Array after moving all zeros is:
+        // "+Arrays.toString(result));
 
         // // Is Subsequence (Medium - 392)
-        // String s = "axc"; String  t = "ahbgdc";
-        // System.out.println("Is \""+s+"\" a subsequence of \""+t+"\"?~ "+isSubsequence(s, t));
+        // String s = "axc"; String t = "ahbgdc";
+        // System.out.println("Is \""+s+"\" a subsequence of \""+t+"\"?~
+        // "+isSubsequence(s, t));
 
+        // // Container With Most Water (Med 11)
+        // int [] heights = {1,8,6,2,5,4,8,3,7};
+        // System.out.println("The maximum water that can be contained by the container
+        // is : "+ maxArea(heights));
 
-        // Container With Most Water  (Med  11)
-        int [] heights = {1,8,6,2,5,4,8,3,7};
-        System.out.println("The maximum water that can be contained by the container is : "+ maxArea(heights));
+        // Max Number of K-Sum Pairs (Med 1679)
+        int k = 5;
+        int[] nums = { 1, 2, 3, 4 };
+        System.out.println("The maximum number of possible operations: " + maxOperations(k, nums));
     }
 }
