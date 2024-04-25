8946b6656f3f160a56d6b9bf47e2b218b198d25f
Patil, Siddhant <sidhantpatil04@gmail.com>
2024-04-25 09:41:40 -0400

    LC Maximum Number of Vowels in a Substring of Given Length (M- 1456)

--- LC/LC75/Sliding_Window/SlidingWindow.java
+++ LC/LC75/Sliding_Window/SlidingWindow.java
@@ -1,3 +1,5 @@
+import java.util.*;
+
 public class SlidingWindow{
 
     // 1. Maximum Average Subarray I (E- 643)
@@ -25,11 +27,48 @@ public class SlidingWindow{
         return (double) maxNum/k;
     }
 
+
+    // 2. Maximum Number of Vowels in a Substring of Given Length (M- 1456)
+    public static int maxVowels(String s, int k) {
+        int maxVow = 0;
+        int inWindowVow = 0;
+
+        Set<Character> vowels = new HashSet<>(Arrays.asList('a', 'e', 'i', 'o', 'u'));
+
+        for (int i=0; i<k; i++){
+            if (vowels.contains(s.charAt(i))){
+                inWindowVow++;
+            }
+        }
+        maxVow = inWindowVow;
+
+        for (int i=k; i<s.length();i++){
+            if (vowels.contains(s.charAt(i-k))){
+                inWindowVow--;
+            }
+
+            if (vowels.contains(s.charAt(i))){
+                inWindowVow++;
+            }
+            maxVow = Math.max(maxVow, inWindowVow);
+            if (maxVow == k) break;
+        }        
+        return maxVow;
+    }
+
+
     public static void main (String[] args) {
 
         // 1. Maximum Average Subarray I (E- 643)
         int [] nums = {1,12,-5,-6,50,3};
         int k = 4;
         System.out.println("The maximum average of given k is: "+ findMaxAverage(nums, k));
+
+
+        // 2. Maximum Number of Vowels in a Substring of Given Length (M- 1456)
+        String s = "abciiidef"; int k2 = 3;
+        System.out.println("The max num of vowels present inside k are: "+ maxVowels(s, k2));
+
+
     }
 }
