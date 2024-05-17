d634b59783f825585ec46f3afd578616f4bef358
Patil, Siddhant <sidhantpatil04@gmail.com>
2024-05-17 17:34:24 -0400

    pushAtBottom Stack

--- Basic DSA/Stack/java/StackColl.java
+++ Basic DSA/Stack/java/StackColl.java
@@ -1,10 +1,26 @@
 import java.util.*;
 public class StackColl {
+
+    public static void pushAtBottom(int val, Stack<Integer>s){
+        if (s.isEmpty()){
+            s.push(val);
+            return;
+        }
+        // get the top elem out.
+        int top = s.pop();
+
+        // call recurssion with a new head. (this will reach the end).
+        pushAtBottom(val, s);
+        // again push the top inside.
+        s.push(top);
+    }
     public static void main(String[] args) {
         Stack<Integer> stack = new Stack<Integer>();
         stack.push(1);
         stack.push(2);
         stack.push(3);
+
+        pushAtBottom(4, stack);
 
         while(!stack.isEmpty()){
             System.out.println(stack.peek());
